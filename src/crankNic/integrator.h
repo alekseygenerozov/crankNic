8ef8b6e1fd27321bfcd45ba55c8a9da39d5becb6
Munier Salem <salem.munier@gmail.com>
2012-06-07 15:00:37 -0400

    and here is the file, haha

---
+++ src/crankNic/integrator.h
@@ -0,0 +1,29 @@
+#include <math.h>
+
+#include "global.h"
+
+/*
+ *	INTEGRATOR
+ *
+ *		by Munier Salem
+ *		July, 2012
+ *
+ *
+ *			Implements Simpson's 1/3rd Rule on a Logarithmic Grid
+ *			For Integrating a Known Integrand (must be sufficiently
+ *			smooth, and evaluable at both end points -- ie closed --).
+ */
+
+double simpsonInt(double* x){
+
+	double result = 0.0,
+		evenCoeff = (2.0*lambda-1.0)/(6.0*lambda*lambda) + (2.0-lambda)/6.0,
+		oddCoeff  = (lambda+1.0)*(lambda+1.0)/(6.0*lambda*lambda);
+
+	result += (2.0-lambda)/6.0*x[0];
+	for(int j=1;j<N-1;j+=2)
+			result += ((j%2==0)?evenCoeff:oddCoeff)*x[j]*pow(lambda,j);
+	result += (2.0*lambda-1.0)/(6.0*lambda)*pow(lambda,N-4)*x[N-1];
+
+	return result;
+}// end simpsonInt 
