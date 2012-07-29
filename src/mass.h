aca5ee4cea28151eade95918ff6ab3f8708b7f87
munierSalem <salem.munier@gmail.com>
2012-07-29 13:26:53 -0400

    some formatting

--- src/mass.h
+++ src/mass.h
@@ -12,16 +12,16 @@
  */
 double massIntegral(const double *l, const double *Fj){
 
-  double result = 0.0,
-    evenCoeff = (2.0*lambda-1.0)/(6.0*lambda*lambda*lambda) + (2.0-lambda)/6.0,
-    oddCoeff  = (lambda+1.0)*(lambda+1.0)/(6.0*lambda*lambda);
+	double result = 0.0,
+		evenCoeff = (2.0*lambda-1.0)/(6.0*lambda*lambda*lambda) + (2.0-lambda)/6.0,
+		oddCoeff  = (lambda+1.0)*(lambda+1.0)/(6.0*lambda*lambda);
 
-  result += (2.0-lambda)/6.0*dmdl(Fj[0],l[0]);
-  for(int j=1;j<N-1;j++)
-      result += ((j%2==0)?evenCoeff:oddCoeff)*dmdl(Fj[j],l[j])*pow(lambda,j);
-  result += (2.0*lambda-1.0)/6.0*pow(lambda,N-4)*dmdl(Fj[N-1],l[N-1]);
+	result += (2.0-lambda)/6.0*dmdl(Fj[0],l[0]);
+	for(int j=1;j<N-1;j++)
+		result += ((j%2==0)?evenCoeff:oddCoeff)*dmdl(Fj[j],l[j])*pow(lambda,j);
+	result += (2.0*lambda-1.0)/6.0*pow(lambda,N-4)*dmdl(Fj[N-1],l[N-1]);
 
-  return result*dl*(lambda+1.0);
+	return result*dl*(lambda+1.0);
 }// end massIntegral
 
 
